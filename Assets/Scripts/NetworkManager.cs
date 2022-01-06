953eadbe1cbf75362edfe26a059f0f6259ffc5c8
Tyler Latimer <trlatimer95@gmail.com>
2022-01-05 21:49:26 -0800

    Imported photon, setup initial NetworkManager that can connect to server and join a room

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined lobby");
+    }
+}
