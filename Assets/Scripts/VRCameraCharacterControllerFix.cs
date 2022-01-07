18fd69208a412d129ed604303e2c87321dbae029
trlatimer <trlatimer95@gmail.com>
2022-01-07 10:04:00 -0800

    Fixed CharacterController not moving with camera

---
+++ Assets/Scripts/VRCameraCharacterControllerFix.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCameraCharacterControllerFix : MonoBehaviour
+{
+    public CharacterController character;
+    public GameObject centerEyeAnchor;
+    private Vector3 place;
+
+    void FixedUpdate()
+    {
+        place = new Vector3(centerEyeAnchor.transform.localPosition.x, 1.0f, centerEyeAnchor.transform.localPosition.z);
+
+        character.center = place;
+    }
+}
